40514237e4250ad967a8686dc6025e61370d4907
swap1408 <96335058+swap1408@users.noreply.github.com>
2022-03-30 10:08:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('CICD App V2!')
+  res.send('Tu gadha aahe')
 })
 
 app.get('/status', (req, res) => {
