21f95a0442bb2fb047ff0fb5f8f2f43c30a6d23d
swap1408 <96335058+swap1408@users.noreply.github.com>
2022-03-31 12:55:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('aws_codebuild_codedeploy_nodeJs_demo')
+  res.send('aws_codebuild_codedeploy_nodeJs')
 })
 
 app.get('/status', (req, res) => {
